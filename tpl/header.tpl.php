f577cb89f5e182758bacbec6caedbec98f6187de
Marc Douguet <marcdouguet@outlook.com>
2017-07-05 18:45:59 +0200

    info

--- tpl/header.tpl.php
+++ tpl/header.tpl.php
@@ -15,12 +15,27 @@
     <div id="info" style="width:1200px;height:1200px;overflow:auto;">
         <h2>MAnaDram</h2>
         <p>MAnaDram est un moteur d'analyse dramaturgique qui recherche dans un corpus de pièces de théâtre tous les groupes de scènes présentant la même séquence d'entrée et de sortie des personnages.</p>
+	<h3>Recherche à l'aide de la matrice</h3>
         <h4>Exemples</h4>
         <p>Dessiner le motif à rechercher en cliquant sur les cases du tableau. Chaque ligne correspond à un personnage, chaque colonne à une scène.</p>
         <p><img src="img/A-AB-A.png"/></p>
         <p>trouvera toutes les séquences de trois scènes, faisant intervenir deux personnages et deux personnages seulement, dont l'un reste présent en permanence, et dont l'autre n'est présent que durant la scène centrale.</p>
-        <p><img src="img/A.png"/></p>
-        <p>trouvera tous les monologues.</p>
+        <h4>Débuts et fins d'acte</h4>
+	<p>Une colonne vide représente un entracte ou le début ou la fin de la pièce.</p>
+        <p><img src="img/A--A.png"/></p>
+	<p>trouvera tous les entractes entourés par deux monologues d'un même personnage.</p>
+	<br/>
+	<p>Quand le motif dessiné ne commence et ne se finit par une colonne vide, la recherche ne prends pas en compte sa position à l'intérieur de la pièce.</p>
+        <p><img src="img/A-AB-B.png"/></p>
+        <p>trouvera toutes les séquences de trois scènes où un personnage prononce un monologue et en rencontre un autre qui, dans un troisième temps, reste à son tour seul sur le plateau, que cette séquence se trouve au milieu, au début ou à la fin d'un acte.</p>
+	<br/>
+	<p>En revanche</p>
+        <p><img src="img/A-AB-.png"/></p>
+	<p>trouvera toutes les séquences où un personnage monologue puis dialogue avec un autre personnage, et qui se situent à la fin d'un acte.</p>
+	<br/>
+	<p>Pour élargir la recherche, réduire le nombre de colonnes de la matrices grâce au bouton <img src="img/delcol.png" style="height:15px;display: inline;margin:0;">.</p>
+        <p><img src="img/A-AB.png"/></p>
+	<p>trouvera toutes les séquences où un personnage monologue puis dialogue avec un autre personnage, quelle que soit sa position dans la pièce. Par conséquent, il est impossible de ne trouver que les occurrences d'un motif donné qui se trouvent au milieu d'un acte, à l'exclusion des occurrences situées au début ou à la fin d'un acte.</p>
         <h4>Grouper les personnages</h4>
         <p><img src="img/A-AB-A-G.png"/></p>
         <p>trouvera toutes les séquences de trois scènes, faisant intervenir deux personnages ou groupes de personnages, dont l'un reste présent en permanence, et dont l'autre n'est présent que durant la scène centrale.</p>
@@ -29,15 +44,16 @@
         <h4>Ignorer les confidents</h4>
         <p><img src="img/A-AB-C.png"/></p>
         <p>trouvera toutes les séquences de deux scènes où un personnage (et un seul) vient en trouver un autre qui était seul sur le plateau, ainsi que celles où un confident est en outre présent durant l'une et/ou l'autre de ces scènes</p>
-        <h4>Requête XPath</h4>
-        <p>Les requêtes sont évaluées à partir de la première scène de chaque séquence correspondant au motif recherché. Le résultat affiché correspond au nombre d'éléments renvoyés par la requête</p>
-        <p>La requête doit respecter la syntaxe XPath 1.0. Les éléments doivent être précédés de l'espace de nom "tei"</p>
-        <ul>
-            <li>./parent::* récupère l'acte courant </li>
-            <li>./tei:sp renvoie le nombre de répliques de la scène</li>
-        </ul>
-        <br/>
-        <h4>Données</h4>
+	<br/>
+	<h3>Recherche par code</h3>
+	<p>Il est possible de rechercher un ou plusieurs motifs par son code en cliquant sur le bouton<i class="fa fa-th tooltip-s"></i>. Les différents motifs doivent être séparés par un saut de ligne.</p>
+	<ul>
+	    <li>Chaque personnage ou groupe de personnage distinct est identifié par une lettre majuscule, selon l'ordre dans lequel ils entrent sur le plateau, puis selon l'ordre inverse dans lequel ils en sortent.</li>
+	    <li>Les scènes sont séparées par /.</li>
+	    <li>Les personnages ou groupes de personnages, à l'intérieur d'une scène, par -.</li>
+	    <li>// indique un entracte.</li>
+	</ul>
+        <h3>Données</h3>
         <ul>
         <li><a href="http://theatre-classique.fr/">Théâtre classique (Paul Fièvre)</a></li>
         <li><a href="http://bibdramatique.paris-sorbonne.fr/">Bibliothèque dramatique (UMR CELLF 16-21)</a></li>
